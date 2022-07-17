cfaab52ad437e484dfa3d3d15c3b1824d7c9c455
GEEZYBOT <108412630+GEEZYBOT@users.noreply.github.com>
2022-07-17 16:39:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
+  global.ownername = 'Geezy' // Ganti Jadi Namamu
+  global.owner = ['6289529904752','6289529904752','6289529904752'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6289529904752' // Owner Utama Istilahnya
+  global.botname = 'GeezyBotz' // Ganti Jadi Nama Botmu
+  global.packname = 'GeezyBotz' // Sticker Weem
+  global.author = 'Geezy' // Sticker Weem
   global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.linkgroup = 'https://chat.whatsapp.com/KGYgCmZPe0HA0qb8RH36H3' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UCOnRpBQILZPMZMCT1WqFGBg' // Ganti Sama Link Yt Mu
+  global.github = '-----' // Ganti Sama Link Github Mu
+  global.tiktok = 'tiktok.com/@geezytripleseven' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -63,7 +63,7 @@
   private: '_Khusus Private Chat_',
   bot: '_Khusus Pengguna Nomer Bot_',
   wait: '_Sabar Sedang Di Proses ..._',
-  done: '_Done Ngab 🤗_',
+  done: '_Done Banhj😅👆🏻_',
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
